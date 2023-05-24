655b23f4c292a7a4511011149be005cd9345105a
Denis <yum-yab-dev@pm.me>
2023-05-24 15:15:47 +0200

    Adds minimal changes

--- leaf_area_measure/README.md
+++ leaf_area_measure/README.md
@@ -1,3 +1,50 @@
 # Leaf area measure script
 
-TODO
+## 1. Preparing the images
+
+First task is to reduce images to the essence, I used gimp for this with the following workflow:
+
+1. Load the image in gimp
+2. Use the `lasso-select tool` for selecting the corners of the A4 paper
+3. Press `CTRL + i` for inverting the selection
+4. Press `CTRL + x` for cutting out everything thats not on the paper
+5. Use `Image > Crop to content` to reduce the image size
+    - this step is optional since alpha channel (= transparency) is ignored in the script, but this reduces file size and therefor makes it a bit faster
+6. Save images in proper format for script:
+    - Name of the file `{leaf-number}_someuniquename_{double}.jpg` (only use `double` if two A4 papers were used)
+    - save all files of one species in one directory
+
+
+## 2. Setting up env
+
+```bash
+git clone https://github.com/yum-yab/ground_truthing_module
+cd leaf_area_measure
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## 3. Actually calculating the sizes
+
+For calculating the sizes of one species run 
+
+```bash
+python leaf_area_measurement.py calculate {path_to_dir}
+```
+
+So for example the following command will calculate all the maple leaf sizes:
+
+```bash
+python leaf_area_measurement.py calculate leaf_images/cropped/ahorn
+```
+
+## 4. Debugging weird measures
+
+For debugging specific images run
+
+```bash
+python leaf_area_measurement.py inspect leaf_images/cropped/ahorn/1_ahorn01.jpg
+```
+
+This will open three images: The original, one in HSV color space and the final binary mask (grey areas here are the transparent parts of the image)
--- leaf_area_measure/leaf_area_measurement.py
+++ leaf_area_measure/leaf_area_measurement.py
@@ -30,67 +30,6 @@ def calculate_color_distance(sourcecolor, targetcolor):
 def load_image(path: str) -> np.ndarray:
     return iio.imread(uri=path)
 
-def hsv_filter_fun(hsv, skipcolors, max_distance: float, skipcolor_val = 0.5) -> float:
-
-    hue, saturation, value = hsv
-    
-    for skipcolor in skipcolors:
-        if calculate_color_distance(hsv, skipcolor) < max_distance:
-            return skipcolor_val
-
-    if saturation < 0.2:
-        # this filters out grey
-        if value < 0.6:
-            # it is grey
-            return 1.0
-        else:
-            # it is white
-            return 1.0
-    else:
-        # it is the leaf
-        return 0.0
-
-def convert_color(pixelcolor, skipcolors, distance_threshold):
-
-    for i, sc in enumerate(skipcolors):
-        if calculate_color_distance(pixelcolor, sc) < distance_threshold:
-            return i + 2
-
-    return skimage.color.rgb2gray(pixelcolor)
-
-
-def convert_hsv_to_greyscale(image: np.ndarray, skipcolors, max_distance) -> np.ndarray:
-    """Converts an image to greyscale. Colors in the skipcolor list will be converted to value 2+index"""
-    
-    result =  np.zeros(image.shape[:2])
-        
-    for i, row in enumerate(image):
-        for j, pixel in enumerate(row):
-            result[i, j] = hsv_filter_fun(pixel, skipcolors, max_distance)
-
-    return result
-
-
-
-def greyscale_easy_convert(image) -> np.ndarray:
-
-    return skimage.color.rgb2gray(image)
-
-def show_image(image, cmap_val=None):
-    fig, ax = plt.subplots()
-    if cmap_val:
-        plt.imshow(image, cmap="gray")
-    else:
-        plt.imshow(image)
-
-def build_histogram(image):
-    histogram, bin_edges = np.histogram(image, bins=256, range=(0.0, 1.0))
-    plt.plot(bin_edges[0:-1], histogram)
-    plt.title("Grayscale Histogram")
-    plt.xlabel("grayscale value")
-    plt.ylabel("pixels")
-    plt.xlim(0, 1.0)
-
 def calculate_area_relation_of_mask(mask: np.ndarray) -> float:
     
 
@@ -114,29 +53,83 @@ def calculate_area_relation_of_mask(mask: np.ndarray) -> float:
 
     return (leaf_pixel_count / total_paper_area) 
 
-def calculate_leaf_area_of_path(path: str, leaf_number, a4_multiplier) -> float:
 
-    baseimage = load_image(path)
-    hsv_image = skimage.color.rgb2hsv(baseimage)
+class LeafAreaMeasurer:
 
-    greyscale_hsv_filtered = convert_hsv_to_greyscale(hsv_image, [ALPHA_VALUE_HSV], 0.1)
+    def __init__(self, saturation_threshold: float = 0.2, skipcolors: List = [ALPHA_VALUE_HSV], max_skipping_distance: float = 0.1) -> None:
+        """Class for calculating the Area of images. 
+        saturation_threshold: Is the threshold at which something is not considered a 'leaf' anymore
+        skipcolors: List of hsv colors (as triple) which are not part of the area (e.g. the alpha channel)
+        max_skipping_distance: Is the distance a pixel can be away from the skipping color to be considered skipping"""
+        
+        self.saturation_threshold = saturation_threshold
+        self.skipcolors = skipcolors
+        self.max_skipping_distance = max_skipping_distance
+
+
+    def hsv_filter_fun(self, hsv_color_pixel, skipcolor_val = 0.5) -> float:
     
-    leaf_area_relation = calculate_area_relation_of_mask(greyscale_hsv_filtered)
-    print(f"Leaf area relation for {path}: {leaf_area_relation * 100} %")
-    return leaf_area_relation * (a4_multiplier * A4_SIZE) 
-
-
-def compare_image_with_mask(path: str):
-
-    baseimage = load_image(path)
-    hsv_image = skimage.color.rgb2hsv(baseimage)
-
-    greyscale_hsv_filtered = convert_hsv_to_greyscale(hsv_image, [ALPHA_VALUE_HSV], 0.1)
+        hue, saturation, value = hsv_color_pixel
+        
+        for skipcolor in self.skipcolors:
+            if calculate_color_distance(hsv_color_pixel, skipcolor) < self.max_skipping_distance:
+                return skipcolor_val
     
-    show_image(baseimage)
-    show_image(hsv_image, cmap_val="hsv")
-    show_image(greyscale_hsv_filtered, cmap_val="grey")
-    plt.show()
+        if saturation < self.saturation_threshold:
+            # this filters out grey
+            if value < 0.6:
+                # it is grey
+                return 1.0
+            else:
+                # it is white
+                return 1.0
+        else:
+            # it is the leaf
+            return 0.0
+
+
+
+    def convert_hsv_to_greyscale(self, image: np.ndarray) -> np.ndarray:
+        """Converts an image to mask of three values: White for paper, black for leafs, grey for not considered area."""
+        
+        result =  np.zeros(image.shape[:2])
+            
+        for i, row in enumerate(image):
+            for j, pixel in enumerate(row):
+                result[i, j] = self.hsv_filter_fun(pixel)
+    
+        return result
+
+    def calculate_leaf_area_of_path(self, path: str, a4_multiplier) -> float:
+    
+        baseimage = load_image(path)
+        hsv_image = skimage.color.rgb2hsv(baseimage)
+    
+        greyscale_hsv_filtered = self.convert_hsv_to_greyscale(hsv_image)
+        
+        leaf_area_relation = calculate_area_relation_of_mask(greyscale_hsv_filtered)
+        print(f"Leaf area relation for {path}: {leaf_area_relation * 100} %")
+        return leaf_area_relation * (a4_multiplier * A4_SIZE) 
+
+    def compare_image_with_mask(self, path: str):
+    
+        baseimage = load_image(path)
+        hsv_image = skimage.color.rgb2hsv(baseimage)
+    
+        greyscale_hsv_filtered = self.convert_hsv_to_greyscale(hsv_image)
+        
+        show_image(baseimage)
+        show_image(hsv_image)
+        show_image(greyscale_hsv_filtered, cmap_val="gray")
+        plt.show()
+
+
+def show_image(image, cmap_val=None):
+    fig, ax = plt.subplots()
+    if cmap_val:
+        plt.imshow(image, cmap=cmap_val)
+    else:
+        plt.imshow(image)
 
 
 def __main():
@@ -144,9 +137,11 @@ def __main():
     command = sys.argv[1]
     
     directory_path = sys.argv[2]
+    
+    leaf_area_measurer = LeafAreaMeasurer()
 
     if command == "inspect":
-       compare_image_with_mask(directory_path) 
+       leaf_area_measurer.compare_image_with_mask(directory_path) 
     elif command == "calculate":
         all_file_paths = [f"{directory_path}/{fname}" for fname in listdir(directory_path) if isfile(f"{directory_path}/{fname}")]
         
@@ -161,7 +156,7 @@ def __main():
 
             leafnum += int(fname.split("_")[0])
 
-            results.append(calculate_leaf_area_of_path(fpath, leafnum, factor))
+            results.append(leaf_area_measurer.calculate_leaf_area_of_path(fpath, factor))
         print(f"Results for {directory_path}: {results}")
         print(f"Number of leafes: {leafnum}")
         print(f"Mean: {sum(results)/leafnum} cm^2")
