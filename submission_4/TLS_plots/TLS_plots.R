565d0ab9460aab2ea32a7c8424b9dab3b487d87c
Denis <yum-yab-dev@pm.me>
2023-07-20 17:56:32 +0200

    Adds first draft of submission 4

--- submission_4/TLS_plots/TLS_plots.R
+++ submission_4/TLS_plots/TLS_plots.R
@@ -14,10 +14,10 @@ library(rayshader)
 library(rgdal)
 library(sf)
 library(ggplot2)
+library(tidyverse)
 
-
-#setwd("/Users/marie-louisekorte/Documents/Uni Leipzig/Semester 2/Ground Truthing/analysis/TLS")
-setwd(getSrcDirectory(function(){})[1])
+setwd("/home/denis/workspace/uni/ground_truthing/submission_4/TLS_plots")
+# setwd(getSrcDirectory(function(){})[1])
 
 ### colorbar 
 col <- height.colors(25)
@@ -25,7 +25,6 @@ col <- height.colors(25)
 ### setting an extent to tls raster
 aoi <- shapefile("shapefile/polygon.shp")
 aoi
-
 
 ### TLS canopy height model
 chm_IEP_new <- raster('chm_IEP.tif')
@@ -72,6 +71,11 @@ hist(h_s2_top)
 
 plot(h_s2_sd)
 hist(h_s2_sd)
+
+chm_residuals_low_res <- h_tls_top_res - h_s2_top
+
+df_residuals_low_res <- as.data.frame(chm_residuals_low_res, xy = TRUE)
+colnames(df_residuals_low_res) <- c("x", "y", "value")
 
 
 ################################################################################################
@@ -152,6 +156,8 @@ ggplot(df_h_tls_sd_res, aes(x = x, y = y, fill = value)) +
        fill = "Standard Deviation")
 ggsave("figures/chm_sd.png", dpi = 300)
 
+
+
 ## sentinel
 df_h_s2_top <- as.data.frame(h_s2_top, xy = TRUE)
 colnames(df_h_s2_top) <- c("x", "y", "value")
@@ -192,10 +198,66 @@ ggsave("figures/sentinel_sd.png", dpi = 300)
 png(file="figures/histograms.png", width=900, height=750, units='px')
 
 par(mfrow=c(2,2))
-hist(h_tls_top_res, main = "TLS Mean Canopy Height", col = '#568fda', ylim = c(0,70), xlim = c(0,35), breaks = 20)
-hist(h_s2_top, main = "Sentinel Mean Canopy Height", col = '#568fda', ylim = c(0,70), xlim = c(0,35), breaks = 1)
-hist(h_tls_sd_res, main = "TLS SD of Canopy Height", col = "#038203", ylim = c(0,120),  xlim = c(0,12), breaks =20)
-hist(h_s2_sd, main = " Sentinel SD of Canopy Height", col = '#038203', ylim = c(0,120),  xlim = c(0,12), breaks = 3)
+hist(h_tls_top_res, main = "TLS Canopy Height distribution", col = '#568fda', ylim = c(0,70), xlim = c(0,35), breaks = 20)
+hist(h_s2_top, main = "Sentinel Canopy Height distribution", col = '#568fda', ylim = c(0,70), xlim = c(0,35), breaks = 1)
+hist(h_tls_sd_res, main = "TLS SD distribution of Canopy Height", col = "#038203", ylim = c(0,120),  xlim = c(0,12), breaks =20)
+hist(h_s2_sd, main = " Sentinel SD distribution of Canopy Height", col = '#038203', ylim = c(0,120),  xlim = c(0,12), breaks = 3)
+
+## residuals
+
+breakpoints <- c(seq(-40, -1, 16), seq(0, 10, 4))
+rb_colors <- c(RColorBrewer::brewer.pal(9, "Reds"), RColorBrewer::brewer.pal(9, "Blues"))
+
+color_scale <- scale_fill_gradientn(
+  colours = colorRampPalette(rev(RColorBrewer::brewer.pal(11, "RdBu")))(255),
+  values = c(1.0, (0 - min(df_residuals_low_res$value)) / (max(df_residuals_low_res$value) - min(df_residuals_low_res$value)), 0)
+)
+
+ggplot(df_residuals_low_res, aes(x = x, y = y, fill = value)) +
+  geom_raster() +
+  # scale_fill_gradientn(colors = rb_colors) +
+  color_scale +
+  theme_minimal() +
+  theme(legend.position = "right", text = element_text(size = 20)) +
+  scale_x_continuous(limits = c(12.30897, 12.30973), expand = c(0, 0),labels = abbreviate) +
+  scale_y_continuous(limits = c(51.36527, 51.36638), expand = c(0, 0)) + 
+  coord_equal() + 
+  labs(title = "Residuals of the CHM",
+       x = "Longitude",
+       y = "Latitude",
+       fill = "value")
+ggsave("figures/residuals_low_res.png", dpi = 300)
+
+ggplot(df_residuals_low_res, aes(x = value)) +
+  geom_histogram() +
+  # scale_fill_gradientn(colors = rb_colors) +
+  color_scale +
+  theme_minimal() +
+  theme(legend.position = "right", text = element_text(size = 20)) +
+  # scale_x_continuous(limits = c(12.30897, 12.30973), expand = c(0, 0),labels = abbreviate) +
+  # scale_y_continuous(limits = c(51.36527, 51.36638), expand = c(0, 0)) + 
+  coord_equal() + 
+  labs(title = "Residuals of the CHM")
+ggsave("figures/residuals_histo.png", dpi = 300)
+
+ggplot(df_residuals_low_res, aes(x = value, color = value)) +
+  geom_histogram() +
+  scale_color_brewer(palette="Paired")
+  # scale_color_brewer(color_scale) +
+  theme_minimal() +
+  theme(legend.position = "right", text = element_text(size = 20)) +
+  labs(title = "Histogram of Residuals of the CHM", x = "Residuals", y = "Frequency")
+ggsave("figures/residuals_histo.png", dpi = 300)
+
+png(file="figures/residuals_histogram.png", width=1500, height=1250, units='px')
+
+hist(chm_residuals_low_res, 
+     main = "Residuals of the CHM comparison", 
+     col = '#568fda', 
+     ylim = c(0,15), 
+     xlim = c(-40,10), 
+     breaks = 20)
+
 
 dev.off()
 ###############################################################################
